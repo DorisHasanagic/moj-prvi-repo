9268b7bfaa553b0d9b1291c2af339e1e2763515e
“Doris <doris.tomicic0211@gmail.com>
2023-11-23 15:06:26 +0100

    test test

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ console.log("poruka od osobe A");
 //git push origin main
 
 //git clone link - link kopiramo sa githuba, kreira se folder
+
+//povlacenje koda - git pull origin main
+
+//
+// git init: Inicijalizira prazan Git repozitorij u trenutnom direktoriju.
+// git clone <url>: Kopira postojeći Git repozitorij s udaljenog servera na lokalno računalo.
+// git add <file>: Stavlja promjene u određenom datoteci u "staging area" kako bi se spremile za commit.
+// git commit -m "Poruka": Sprema promjene koje su dodane u "staging area" u repozitorij s odgovarajućom porukom.
+// git status: Prikazuje stanje radnog direktorija i "staging area", ukazujući na promjene koje nisu spremljene.
+// git log: Prikazuje povijest commitova, uključujući informacije poput autora, datuma i poruke.
+// git branch: Prikazuje dostupne grane i označava trenutnu granu.
+// git checkout <branch>: Pomiče HEAD na određenu granu.
+// git merge <branch>: Spaja odabranu granu s trenutnom granom.
+// git pull: Dohvaća najnovije promjene s udaljenog repozitorija i spaja ih s lokalnim promjenama.
+// git push: Šalje lokalne promjene na udaljeni repozitorij.
+// git remote -v: Prikazuje popis udaljenih repozitorija povezanih s trenutnim repozitorijem.
+// git fetch: Dohvaća sve promjene s udaljenog repozitorija, ali ih ne spaja s trenutnim radnim direktorijem.
+// git reset <commit>: Poništava promjene koje su napravljene nakon određenog commita.
+// git stash: Privremeno pohranjuje nespremljene promjene kako biste mogli raditi na drugoj grani.
+// git tag <tagname>: Označava određeni commit s određenim tagom.
